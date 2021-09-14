a9aa6a951b4ab8318196a602e8f4f66b216d28a5
SeungHyeonTak-sub <conficker77@gmail.com>
2021-09-15 01:17:35 +0900

    - jsp servlet 생성 및 작성

--- src/com/testPrj/MainServlet.java
+++ src/com/testPrj/MainServlet.java
@@ -1,13 +1,23 @@
+package com.testPrj;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "MainServlet", value = "/MainServlet")
+@WebServlet("/ms")
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter out = response.getWriter();
+        out.print("<html>");
+        out.print("<head>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<p>Hello Servlet!!</p>");
+        out.print("</body>");
+        out.print("</html>");
     }
 
     @Override
